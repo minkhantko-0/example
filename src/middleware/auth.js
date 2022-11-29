75bb8736d1f7b36f00a938870df281291a978205
minkhantkoo <minkhant5771@gmail.com>
2022-11-30 00:14:24 +0630

    miss-typed env field name fixed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const Owner = require("../models/owner");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, process.env.JWT_SECERT);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const owner = await Owner.findOne({
       _id: decoded._id,
       "tokens.token": token,
